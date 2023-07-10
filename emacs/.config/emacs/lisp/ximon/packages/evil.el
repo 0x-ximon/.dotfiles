0170504ff7739a44ca68dc1fc19ad07ce0d75f20
Simon <salviensky@gmail.com>
2023-07-10 22:09:41 +0100

    feat: added "jk" keybindings for Emacs

--- emacs/.config/emacs/lisp/ximon/packages/evil.el
+++ emacs/.config/emacs/lisp/ximon/packages/evil.el
@@ -1,6 +1,23 @@
 (use-package evil
              :ensure t)
 
+(use-package key-chord
+             :ensure t)
+
 (evil-mode 1)
+
+(key-chord-mode 1)
+
+;; ;; set leader key in all states
+;; (evil-set-leader nil (kbd "C-SPC"))
+;;
+;; ;; set leader key in normal state
+;; (evil-set-leader 'normal (kbd "SPC"))
+;;
+;; ;; set local leader
+;; (evil-set-leader 'normal "," t)
+
+(setq key-chord-two-keys-delay 0.1)
+(key-chord-define evil-insert-state-map "jk" 'evil-normal-state)
 
 (provide 'evil)
