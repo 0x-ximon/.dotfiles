45d31c5922fa9b5c6c2ec3ded642ddda3a205a55
Simon <salviensky@gmail.com>
2023-07-10 18:27:51 +0100

    feat: setup text-wrap and indentation

--- emacs/.config/emacs/ftplugin/org-config.el
+++ emacs/.config/emacs/ftplugin/org-config.el
@@ -4,4 +4,9 @@
 
 (setq-default org-enforce-todo-dependencies t)
 
+(setq org-adapt-indentation t)
+(setq org-startup-indented t)
+
+(add-hook 'text-mode-hook 'visual-line-mode)
+
 (provide 'org-config)
