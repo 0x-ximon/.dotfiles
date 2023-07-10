3eecd409ebb517ee9c707560485e4af17c591aac
Simon <salviensky@gmail.com>
2023-07-10 21:50:19 +0100

    feat: added LSP UI package for emacs

--- emacs/.config/emacs/lisp/ximon/packages/lsp/lsp-mode-config.el
+++ emacs/.config/emacs/lisp/ximon/packages/lsp/lsp-mode-config.el
@@ -8,17 +8,11 @@
                     (lsp-mode . lsp-enable-which-key-integration))
              :commands lsp)
 
-;; optionally
-;; (use-package lsp-ui :commands lsp-ui-mode)
-;; ;; if you are helm user
-;; (use-package helm-lsp :commands helm-lsp-workspace-symbol)
-;; ;; if you are ivy user
-;; (use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
-;; (use-package lsp-treemacs :commands lsp-treemacs-errors-list)
+(setq lsp-headerline-breadcrumb-enable nil)
 
-;; ;; optionally if you want to use debugger
-;; (use-package dap-mode)
-;; ;; (use-package dap-LANGUAGE) to load the dap adapter for your language
+(use-package lsp-ui 
+             :commands lsp-ui-mode)
+
 
 ;; optional if you want which-key integration
 (use-package which-key
