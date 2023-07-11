1b1605583e11605f077c79f74efb0c7f23086895
Simon <salviensky@gmail.com>
2023-07-11 08:13:34 +0100

    fix: used emacs dashboard as startup buffer

--- emacs/.config/emacs/lisp/ximon/packages/doom-dashboard.el
+++ emacs/.config/emacs/lisp/ximon/packages/doom-dashboard.el
@@ -6,6 +6,7 @@
 (setq 
   dashboard-startup-banner "~/.dotfiles/emacs/.config/emacs/assets/logo.txt")
 
+(setq initial-buffer-choice (lambda () (get-buffer-create "*dashboard*")))
 (setq dashboard-banner-logo-title nil)
 (setq dashboard-show-shortcuts nil)
 (setq dashboard-center-content t)
