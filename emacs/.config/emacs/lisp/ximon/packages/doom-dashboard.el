7f5978aab9265266e6bc186fbcc7dfe3b8256e71
Simon Samuel <salviensky@gmail.com>
2024-04-15 19:04:28 +0100

    chore: improved dashboard UI

--- emacs/.config/emacs/lisp/ximon/packages/doom-dashboard.el
+++ emacs/.config/emacs/lisp/ximon/packages/doom-dashboard.el
@@ -1,16 +1,34 @@
 (use-package dashboard
              :ensure t
+
              :config
-             (dashboard-setup-startup-hook))
+             (dashboard-setup-startup-hook)
 
-(setq 
-  dashboard-startup-banner "~/.dotfiles/emacs/.config/emacs/assets/logo.txt")
+             :custom
+             (dashboard-startup-banner "~/.dotfiles/emacs/.config/emacs/assets/logo.txt")
 
-(setq initial-buffer-choice (lambda () (get-buffer-create "*dashboard*")))
-(setq dashboard-banner-logo-title nil)
-(setq dashboard-show-shortcuts nil)
-(setq dashboard-center-content t)
+             (dashboard-banner-logo-title nil)
 
-(setq dashboard-set-footer nil)
+             (dashboard-show-shortcuts t)
 
+             (dashboard-center-content t)
+             (dashboard-vertically-center-content t)
+
+             (dashboard-startupify-list '(dashboard-insert-banner
+                                           dashboard-insert-items
+                                           dashboard-insert-init-info))
+
+             (dashboard-items '((recents   . 5)))
+
+             (dashboard-item-shortcuts '((recents   . "r")))
+
+             (dashboard-display-icons-p t)
+             (dashboard-icon-type 'all-the-icons)  
+             (dashboard-set-heading-icons t)
+
+             (dashboard-set-file-icons t))
+
+
+
+(setq initial-buffer-choice (lambda () (get-buffer-create dashboard-buffer-name)))
 (provide 'doom-dashboard)
