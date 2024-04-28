7fb26024bd820825d7a408f9b2e0462af3e65360
Simon Samuel <salviensky@gmail.com>
2024-04-28 18:59:04 +0100

    Update keybindings for splitting windows and saving buffer in evil mode

--- nvim/.config/nvim/lua/ximon/core/keymaps.lua
+++ nvim/.config/nvim/lua/ximon/core/keymaps.lua
@@ -25,19 +25,19 @@ keymap.set("n", "<leader>+", "<C-a>")
 keymap.set("n", "<leader>-", "<C-x>")
 
 -- Window Splitting
-keymap.set("n", "<leader>sv", "<C-w>v") -- Split Vertically
-keymap.set("n", "<leader>sh", "<C-w>s") -- Split Horizontally
-keymap.set("n", "<leader>se", "<C-w>=") -- Split Windows Equally
-keymap.set("n", "<leader>sx", ":close<CR>") -- Close current split window
+keymap.set("n", "<leader>sv", "<C-w>v")
+keymap.set("n", "<leader>sh", "<C-w>s")
+keymap.set("n", "<leader>se", "<C-w>=")
+keymap.set("n", "<leader>sx", ":close<CR>")
 
 -- Tabs Keymaps
-keymap.set("n", "<leader>to", ":tabnew<CR>") -- Open New Tab
-keymap.set("n", "<leader>tx", ":tabclose<CR>") -- Close Current Tab
-keymap.set("n", "<leader>tn", ":tabn<CR>") -- Go to Next Tab
-keymap.set("n", "<leader>tp", ":tabp<CR>") -- Go to Previous Tab
+keymap.set("n", "<leader>to", ":tabnew<CR>")
+keymap.set("n", "<leader>tx", ":tabclose<CR>")
+keymap.set("n", "<leader>tn", ":tabn<CR>")
+keymap.set("n", "<leader>tp", ":tabp<CR>")
 
 -- Helpful Keymaps
-keymap.set("n", ";w", ":w!<CR>")
+keymap.set("n", ";w", ":update<CR>")
 
 -- Terminal Keymaps
 keymap.set("n", ";t", ":vs | te<CR>")
