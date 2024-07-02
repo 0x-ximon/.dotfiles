6cac0a8bb9d4633f84026ee884920d402d601ec2
Simon Samuel <salviensky@gmail.com>
2024-07-02 09:17:00 +0100

    Update colorscheme to github_dark_default and github_light_default

--- nvim/.config/nvim/lua/ximon/lazy.lua
+++ nvim/.config/nvim/lua/ximon/lazy.lua
@@ -35,11 +35,11 @@ local plugins = {
 			update_interval = 1000,
 			set_dark_mode = function()
 				vim.api.nvim_set_option_value("background", "dark", {})
-				vim.cmd("colorscheme github_dark_tritanopia")
+				vim.cmd("colorscheme github_dark_default")
 			end,
 			set_light_mode = function()
 				vim.api.nvim_set_option_value("background", "light", {})
-				vim.cmd("colorscheme github_light_tritanopia")
+				vim.cmd("colorscheme github_light_default")
 			end,
 		},
 	},
