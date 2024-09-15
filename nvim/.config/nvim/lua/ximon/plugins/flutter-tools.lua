37377168ab00f5b93b49725d8a015156e38decd4
Simon Samuel <salviensky@gmail.com>
2024-09-15 11:02:30 +0100

    chore: cleaned off needless plugins

--- nvim/.config/nvim/lua/ximon/plugins/flutter-tools.lua
+++
@@ -1,8 +0,0 @@
-local status, flutter_tools = pcall(require, "flutter-tools")
-if not status then
-	print("flutter tools not configured correctly")
-end
-
-flutter_tools.setup({
-	flutter_lookup_cmd = "dirname $(which flutter)",
-})
--- nvim/.config/nvim/lua/ximon/plugins/lsp/mason.lua
+++ nvim/.config/nvim/lua/ximon/plugins/lsp/mason.lua
@@ -3,28 +3,28 @@ local status2, mason_lspconfig = pcall(require, "mason-lspconfig")
 local status3, mason_null_ls = pcall(require, "mason-null-ls")
 
 if not status1 then
-	print("Mason is mad")
+    print("Mason is mad")
 end
 if not status2 then
-	print("Mason config not working")
+    print("Mason config not working")
 end
 if not status3 then
-	print("Mason null-ls isn't working")
+    print("Mason null-ls isn't working")
 end
 
 mason.setup()
 
 mason_lspconfig.setup({
-	ensure_installed = {
-		"tsserver",
-		"pyright",
-	},
+    ensure_installed = {
+        "tsserver",
+        "pyright",
+    },
 })
 
 mason_null_ls.setup({
-	ensure_installed = {
-		"prettier",
-		"black",
-		"eslint_d",
-	},
+    ensure_installed = {
+        "prettier",
+        "black",
+        "eslint_d",
+    },
 })
