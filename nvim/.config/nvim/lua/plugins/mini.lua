4a66b810a01b63cf7e9c00b6a0e9f521f97ac080
Simon Samuel <salviensky@gmail.com>
2025-07-17 17:15:07 +0100

    feat: update settings for dotenv and dotnet formatting; refactor mini.nvim plugin configuration

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -3,17 +3,6 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons" },
     version = false,
 
-    keys = {
-        {
-            "~",
-            function()
-                if not MiniFiles.close() then
-                    MiniFiles.open()
-                end
-            end,
-        },
-    },
-
     config = function()
         require("mini.statusline").setup()
         require("mini.sessions").setup()
@@ -21,17 +10,5 @@ return {
         require("mini.icons").setup()
         require("mini.diff").setup()
         require("mini.git").setup()
-
-        -- Mini.Files
-        require("mini.files").setup({
-            mappings = {
-                go_in_plus = "<CR>",
-                go_out_plus = "~",
-                synchronize = "s",
-            },
-            options = {
-                use_as_defualt_explorer = false,
-            },
-        })
     end,
 }
