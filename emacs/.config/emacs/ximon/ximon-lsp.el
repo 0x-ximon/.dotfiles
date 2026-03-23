019cdb15e13ecc8baf4e4a154899ab94ebadd416
Simon Samuel <salviensky@gmail.com>
2026-03-23 15:29:39 +0100

    fix: added react support for emacs

--- emacs/.config/emacs/ximon/ximon-lsp.el
+++ emacs/.config/emacs/ximon/ximon-lsp.el
@@ -9,20 +9,19 @@
 (use-package gleam-ts-mode :ensure t :mode (rx ".gleam" eos))
 
 (use-package haskell-mode :ensure t)
-(use-package typescript-mode :ensure t)
 
 (use-package markdown-mode :ensure t)
 
 (use-package eglot
   :ensure nil
   :hook
-  ((c++-mode csharp-mode c-mode java-mode haskell-mode typescript-mode markdown-mode text-mode) . eglot-ensure)
-
+  ((c++-mode csharp-mode c-mode circom-mode java-mode haskell-mode markdown-mode text-mode typescript-ts-mode tsx-ts-mode) . eglot-ensure)
+  
   :custom
-  (eglot-events-buffer-size 0)                                 ;; No event buffers (Lsp server logs)
-  (eglot-autoshutdown t)                                       ;; Shutdown unused servers.
-  (eglot-report-progress nil)                                  ;; Disable lsp server logs (Don't show lsp messages at the bottom, java)
-  (eglot-ignored-server-capabilities '(:inlayHintProvider))    ;; Disable Inlay Hints
+  (eglot-events-buffer-size 0)
+  (eglot-autoshutdown t)
+  (eglot-report-progress nil)
+  (eglot-ignored-server-capabilities '(:inlayHintProvider))
   
   :config
   (add-to-list 'eglot-server-programs
@@ -37,12 +36,15 @@
   (add-to-list 'eglot-server-programs
 			   '(haskell-mode . ("haskell-language-server-wrapper" "--lsp")))
   (add-to-list 'eglot-server-programs
-			   '(typescript-mode . ("typescript-language-server" "--stdio")))
+			   '((typescript-ts-mode tsx-ts-mode) . ("typescript-language-server" "--stdio")))
 
   (add-to-list 'eglot-server-programs
 			   '(markdown-mode . ("markdown-oxide")))
   (add-to-list 'eglot-server-programs
 			   '(text-mode . ("harper-ls" "--stdio"))))
+
+(add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-ts-mode))
+(add-to-list 'auto-mode-alist '("\\.tsx\\'" . tsx-ts-mode))
 
 (setq-default eglot-workspace-configuration
 			  '(:harper-ls (:linters 
